22aecd57a3d189db0b77112b2b70ea539347888b
Dan Cassidy <5643061+rux616@users.noreply.github.com>
2022-07-17 18:56:05 +1200

    Add SPDX identifier and split file location info

--- Tools/lib/Crc32.cs
+++ Tools/lib/Crc32.cs
@@ -1,8 +1,13 @@
-// Note: File lives at https://github.com/damieng/DamienGKit/blob/178992cb6215104a9312720881d8465d7bf040ba/CSharp/DamienG.Library/Security/Cryptography/Crc32.cs
+// file retrieved from:
+// https://github.com/damieng/DamienGKit/blob/178992cb6215104a9312720881d8465d7bf040ba/CSharp/DamienG.Library/Security/Cryptography/Crc32.cs
+
 
 // Copyright (c) Damien Guard.  All rights reserved.
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+// SPDX-License-Identifier: Apache-2.0
+
 
 using System;
 using System.Collections.Generic;

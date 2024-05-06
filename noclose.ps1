8451f7747a669f04c8d5ab96f150b7c0e5e99cb1
Dan Cassidy <5643061+rux616@users.noreply.github.com>
2024-05-06 17:00:54 +1200

    correct path on noclose script

--- noclose.ps1
+++ noclose.ps1
@@ -1,1 +1,1 @@
-powershell.exe -NoExit .\Installer.ps1 -SkipRepackValidation -SkipChoosingPatchedBa2Dir
+powershell.exe -NoExit .\Tools\lib\Installer.ps1 -SkipRepackValidation -SkipChoosingPatchedBa2Dir
